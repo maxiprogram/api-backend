42b73371e9fa30ef92b1fa0ed0be2c723206947d
MaxiProgram <maxiprogram@gmail.com>
2023-11-24 00:05:43 +0300

    add nodemailer ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as Nodemailer from 'nodemailer';
 
 @Controller('api')
 export class AppController {
@@ -12,6 +13,30 @@ export class AppController {
 
   @Get('send-mail')
   sendMail() {
+    // let transporter = Nodemailer.createTransport('gmail', {
+    //   auth: {
+    //     user: 'youremail@gmail.com',
+    //     pass: 'yourpassword'
+    //   }
+    // });
+
+    let transporter = Nodemailer.createTransport();
+
+    let mailOptions = {
+      from: 'youremail@gmail.com',
+      to: 'maxiprogram@gmail.com',
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
     return 'sendMail';
   }
 
